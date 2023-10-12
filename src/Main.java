c63662549459dd3c080dfa0fb1ca2768335c58ef
Лаисат <lz_256@mail.ru>
2023-10-12 22:37:14 +0300

    Homework task 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
-
 
         public static void task1() {
         var dog = 0.8;
@@ -11,5 +11,13 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+    }
+    public static void task2() {
+        var dog = 0.8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog +4 );
+        System.out.println(cat + 4);
+        System.out.println(paper +4 );
     }
 }
